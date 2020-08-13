51b1d9d2743326cd05a5eb3dea1d9b72cd100e99
inesh.shanaka@gmail.com <[inesh.shanaka@gmail.com]>
2020-08-13 22:00:43 +0100

    File Uplad

--- Default.aspx
+++ Default.aspx
@@ -6,38 +6,20 @@
         <h1>File Upload</h1>
         <p class="lead">Please Select a file to Upload To The Database
         </p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Upload File &raquo;</a></p>
+         <table style="width:100%">    
+            <tr>    
+                <td style="width:30%">    
+                   <h2>Select File</h2>
+                </td>    
+                <td style="width:30%">    
+                    <asp:FileUpload ID="FileUpload1" runat="server" class="btn btn-primary btn-lg" />    
+                </td>    
+                <td>    
+                </td>    
+                <td style="width:40%">    
+                    <asp:Button ID="Button1" class="btn btn-primary btn-lg" runat="server" Text="Upload" OnClick="Button1_Click_FileUpload" />    
+                </td>    
+            </tr>    
+        </table>
     </div>
-
-<%--    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>--%>
-
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,93 @@ namespace Gridbeyond
 {
     public partial class _Default : Page
     {
+        SqlConnection con;
+        string sqlconn;
+    
+        private void connection()
+        {
+            sqlconn = System.Configuration.ConfigurationManager.ConnectionStrings["SqlComunication"].ConnectionString;
+            con = new SqlConnection(sqlconn);
+
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void Button1_Click_FileUpload(object sender, EventArgs e)
+        {
+            //Creating object of datatable  
+            DataTable tblcsv = new DataTable();
+            //creating columns  
+            tblcsv.Columns.Add("Date");
+            tblcsv.Columns.Add("Time");
+            tblcsv.Columns.Add("Price");
+
+            //getting full file path of Uploaded file  
+            string CSVFilePath = Path.GetFullPath(FileUpload1.PostedFile.FileName);
+            //Reading All text  
+            string ReadCSV = File.ReadAllText(CSVFilePath);
+            //spliting row after new line  
+
+            int DataRowCount = 0;
+            foreach (string csvRow in ReadCSV.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(csvRow))
+                {                                  
+                    int count = 0;
+
+                    if (DataRowCount != 0)
+                    {
+                        //Adding each row into datatable  
+                        tblcsv.Rows.Add();
+
+                        foreach (string FileRec in csvRow.Split(','))
+                        {
+
+                            if (count == 0)
+                            {
+                                foreach (string FileRecDate in FileRec.Split(' '))
+                                {
+                                    tblcsv.Rows[tblcsv.Rows.Count - 1][count] = FileRecDate;                                   
+                                    count++;
+                                }  
+                                
+                                if (count == 1)
+                                {
+                                    tblcsv.Rows[tblcsv.Rows.Count - 1][count] = "00:00";
+                                    count++;
+                                }
+                            }
+
+                            count = 2;
+                            tblcsv.Rows[tblcsv.Rows.Count - 1][count] = FileRec;
+
+                            count++;                           
+                        }
+                    }
+                    DataRowCount++;
+                }
+            }
+            //Calling insert Functions  
+            InsertCSVRecords(tblcsv);
+        }
+        //Function to Insert Records  
+        private void InsertCSVRecords(DataTable csvdt)
+        {
+            connection();
+            //creating object of SqlBulkCopy    
+            SqlBulkCopy objbulk = new SqlBulkCopy(con);
+            //assigning Destination table name    
+            objbulk.DestinationTableName = "Market_Price_EX";
+            //Mapping Table column    
+            objbulk.ColumnMappings.Add("Date", "Date");
+            objbulk.ColumnMappings.Add("Time", "Time");
+            objbulk.ColumnMappings.Add("Price", "Price");
+
+            //inserting Datatable Records to DataBase    
+            con.Open();
+            objbulk.WriteToServer(csvdt);
+            con.Close();
         }
     }
 }
